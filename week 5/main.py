cad2631e89a92ad5480bb0a5e6c8f3d86afb6cea
Mouni840 <125848775+Mouni840@users.noreply.github.com>
2023-07-02 19:50:42 +0530

    Week-5

---
+++ week 5/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request, Form, status
+from fastapi.templating import Jinja2Templates
+from models.users import UserModel
+from fastapi.responses import RedirectResponse
+import os
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="html_templates")
+
+if not os.path.exists("static"):
+    os.makedirs("static")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+userBirthDays = []
+
+
+@app.get('/')
+def root(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request, "birthdays": userBirthDays})
+
+
+@app.post('/save_form_data')
+def save_form_data(name: str = Form(...), year: str = Form(...), month: str = Form(...), day: str = Form(...)):
+    # userBirthDays.append(UserModel(name=name, year=year, month=month, day=day))
+    # print(userBirthDays)
+    # print(name,year,month,day)
+
+    data = UserModel(name=name, year=year, month=month, day=day)
+    userBirthDays.append(data)
+    print(data)
+    print(userBirthDays)
+    return RedirectResponse(url=app.url_path_for("root"), status_code=status.HTTP_303_SEE_OTHER)
